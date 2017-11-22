d4b81ce936ce52c48981e513b823b389865e122a
Olivier Mengué <dolmen@cpan.org>
2017-11-22 10:57:49 +0100

    FixDoubleUTF8: improve Fuzz function
    Detect inconsistency between FixDoubleUTF8() and utf8.Valid()

--- fuzz.go
+++ fuzz.go
@@ -5,7 +5,32 @@
 
 package mojibake
 
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
 func Fuzz(data []byte) int {
-	FixDoubleUTF8(data)
-	return 0
+	if len(data) > 20 {
+		return -1
+	}
+
+	isValidUTF8 := utf8.Valid(data)
+
+	out, err := FixDoubleUTF8(data)
+
+	if (err != ErrUTF8) != isValidUTF8 {
+		panic(fmt.Sprintf("UTF-8 validation error: %s", err))
+	}
+
+	switch {
+	case len(data) > 17:
+		return -1
+	case err == nil && len(out) == len(data):
+		return 0
+	case len(data) < 8: // priority to short strings
+		return 1
+	default:
+		return 0
+	}
 }
